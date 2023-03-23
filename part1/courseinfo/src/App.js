81ec046e221ee9247d463a02869eeeafcaf18db4
jholloway <jholloway@griddynamics.com>
2023-03-23 13:42:46 -0500

    refactor to follow 1.4 instructions

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,17 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => {
     return (<div>
@@ -28,25 +30,20 @@ const App = () => {
   }
 
   const Content = (props) => {
+    const allParts = parts.map(part => {
+      return <Part 
+        partName = {part.name}
+        numOfExcercises = {part.exercises}
+      />
+    })
     return (<>
-      <Part
-        partName={props.part1.name}
-        numOfExcercises={props.part1.exercises}
-      />
-      <Part
-        partName={props.part2.name}
-        numOfExcercises={props.part2.exercises}
-      />
-      <Part
-        partName={props.part3.name}
-        numOfExcercises={props.part3.exercises}
-      />
+      {allParts}
     </>)
   }
 
   const Total = (props) => {
     return (<>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>)
   }
 
@@ -54,15 +51,10 @@ const App = () => {
     <div>
       <Header course={course}/>
       <Content 
-        part1={part1} 
-        part2={part2}
-        part3={part3} 
-        
+         parts={parts} 
       />
       <Total 
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+       parts={parts}
       />
     </div>
   )
