466065ff2566627f6ec32142b51f2409df3d1ba2
jholloway <jholloway@griddynamics.com>
2023-03-23 12:23:20 -0500

    complete 1.1 and 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,67 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return (<div>
+      <h1>{props.course}</h1>
+    </div>)
+  }
+
+  const Part = (props) => {
+    return (
+      <>
+        <p>{props.partName} {props.numOfExcercises}</p>
+      </>
+    )
+  }
+
+  const Content = (props) => {
+    return (<>
+      <Part
+        partName={part1}
+        numOfExcercises={exercises1}
+      />
+      <Part
+        partName={part2}
+        numOfExcercises={exercises2}
+      />
+      <Part
+        partName={part3}
+        numOfExcercises={exercises3}
+      />
+    </>)
+  }
+
+  const Total = (props) => {
+    return (<>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </>)
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+        part1={part1} 
+        exercises1={exercises1}
+        part2={part2} 
+        exercises2={exercises2}
+        part3={part3} 
+        exercises3={exercises3}
+      />
+      <Total 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  )
+}
+
+export default App
