e071b03648e15a7a2d55f81ab4ce810a4b0d8777
jholloway <jholloway@griddynamics.com>
2023-03-27 14:08:38 -0500

    2.1 completed - Content, Part, Header and Course components created

---
+++ part2/courseinfo2/src/components/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Part from './Part';
+
+const Content = ({parts}) => {
+    const allParts = parts.map(part => 
+    <Part 
+        key={part.id} 
+        name={part.name} 
+        exercises={part.exercises}
+    />)
+    return (
+        <div>
+            {allParts}
+        </div>
+    );
+};
+
+export default Content;
