5a416ad4563632f54ee7d3ec2b15fbc7e34c1f7c
jholloway <jholloway@griddynamics.com>
2023-03-27 16:50:03 -0500

    2.6 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,31 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) =>{
+    event.preventDefault()
+    const newPerson = {name : newName}
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  console.log(newName , persons)
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
