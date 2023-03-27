6a668d5f4a070c300935e8754aad214e5b09c5fb
jholloway <jholloway@griddynamics.com>
2023-03-27 11:05:39 -0500

    exercise 1.6 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,39 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => {
+  return (<button onClick={handleClick}>
+    {text}
+  </button>)
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () =>{
+    setGood(prevGood => prevGood + 1)
+  }
+
+  const handleNeutral = () =>{
+    setNeutral(prevNeutral => prevNeutral + 1)
+  }
+
+  const handleBad = () =>{
+    setBad(prevBad => prevBad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h3>give feedback</h3>
+      <Button handleClick={handleGood} text="good"/>
+      <Button handleClick={handleNeutral} text="neutral"/>
+      <Button handleClick={handleBad} text="bad"/>
+      <h3>statistics</h3>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
