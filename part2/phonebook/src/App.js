41a994086255bcc01cddc8c4ac7648033f71ce50
jholloway <jholloway@griddynamics.com>
2023-03-28 15:27:34 -0500

    2.11 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
@@ -18,6 +14,10 @@ const App = () => {
   const filteredPersons = filteredPerson.map(person => <p key={person.id}>{person.name} {person.number}</p>)
 
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data))
+  })
   const addPerson = (event) =>{
     event.preventDefault()
     const found = persons.find(person => person.name === newName)
