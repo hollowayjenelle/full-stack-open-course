0ee25b8ec37bb0e7c5412303c1ac717a4e8bbc92
jholloway <jholloway@griddynamics.com>
2023-03-28 10:55:00 -0500

    2.8 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' ,  number: '040-1234567', id: 1}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) =>{
     event.preventDefault()
@@ -12,14 +13,19 @@ const App = () => {
     if (found){
       alert(`${newName} is already added to the phonebook`)
     }else{
-      const newPerson = {name : newName}
+      const newId = persons[persons.length - 1].id + 1
+      const newPerson = {name : newName, number: newNumber, id: newId}
       setPersons(persons.concat(newPerson))
     }
     setNewName('')
   }
 
-  const handleChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   console.log(newName , persons)
@@ -28,14 +34,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
